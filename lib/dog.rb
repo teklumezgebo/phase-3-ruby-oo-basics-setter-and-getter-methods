d7c9bf2e574cd739ed3536073c3a89259d0bbaa8
teklumezgebo <mezge002@umn.edu>
2023-05-11 19:42:07 -0500

    commit tree 860df9a1afccf7b938c372d299edd2c9736b73b5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(arg)
+        @name = arg
+    end
 
+    def name 
+        @name
+    end
+
+    def breed=(arg)
+        @breed = arg
+    end
+
+    def breed
+        @breed
+    end
 end
